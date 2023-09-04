4ba27b2fac01f9ed80ce7721a4cad38ebb624d8f
meeroTheo <meerbalochandhammy12@gmail.com>
2023-09-04 15:01:54 -0400

    ADD: portfolio landing image

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,4 +1,15 @@
+import Image from 'react-bootstrap/Image';
 const Home = ({ name, paragraph }) => {
+  const style = {
+    borderStyle: 'solid',
+    borderWidth: '5px',
+    borderColor: 'black',
+    transition: 'transform 0.5s ease',
+    width: '24rem',
+    height:'auto',
+    boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.2)'
+    
+  }
   return (
     <div className="homeContainer">
       <div className="homeHeadWrapper">
@@ -6,9 +17,10 @@ const Home = ({ name, paragraph }) => {
           Hi, I'm <br />
           <span> {name}. </span>
         </h1>
-        <p>{paragraph} </p>
       </div>
-      <div className="imageContainer">temp image</div>
+      <div className="imageContainer">
+        <Image src="assets/self.jpg" alt="Image Description" className='portraitImage' style={style} roundedCircle/>
+        </div>
     </div>
   );
 };
